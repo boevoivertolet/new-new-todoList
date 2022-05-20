343209b4aa4bee602158d446304f55a2d60fb717
boevoivertolet <bjha78rus@yandex.ru>
2022-05-20 16:05:56 +0300

    02

--- todolist/src/App.tsx
+++ todolist/src/App.tsx
@@ -1,20 +1,28 @@
 import React from 'react';
 import './App.css';
+import {TasksType, TodoList} from './TodoList';
 
 function App() {
+
+    let task1: Array<TasksType> = [
+        {id:1, title:'CSS&HTML', isDone: true},
+        {id:2, title: 'HTML', isDone: true},
+        {id:3, title: 'React', isDone: false}
+    ]
+    let task2: Array<TasksType> = [
+        {id:4, title:'Terminator', isDone: true},
+        {id:5, title: 'XXX', isDone: false},
+        {id:6, title: 'Transformers', isDone: false}
+    ]
+
+
     return (
         <div className="App">
-            <TodoList/>
+            <TodoList title={'What To learn'} tasks={task1}/>
+            <TodoList title={'Movies'} tasks={task2}/>
         </div>
     );
 }
 
 export default App;
 
-function TodoList() {
-    return(
-        <div>
-            TodoList
-        </div>
-    )
-}
