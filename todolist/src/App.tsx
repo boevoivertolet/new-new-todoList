8594891563b9377994a573aa1e2fd0aa8ed20149
boevoivertolet <bjha78rus@yandex.ru>
2022-05-20 14:26:34 +0300

    01

---
+++ todolist/src/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+            <TodoList/>
+        </div>
+    );
+}
+
+export default App;
+
+function TodoList() {
+    return(
+        <div>
+            TodoList
+        </div>
+    )
+}
