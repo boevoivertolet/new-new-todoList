4c4b26c782ec4f96de09645d0c80fbc4060f3e07
boevoivertolet <bjha78rus@yandex.ru>
2022-07-07 16:40:41 +0300

    add salaryReducer test

--- todolist/src/tests/tasks.test.ts
+++ todolist/src/tests/tasks.test.ts
@@ -1,4 +1,4 @@
-import { sum } from './tasks'
+import { ActionType, salaryReducer, StateType, sum } from './tasks'
 import { sub } from './tasks'
 import { div } from './tasks'
 import { mult } from './tasks'
@@ -6,7 +6,7 @@ import { mult } from './tasks'
 
 
 
-test('sum', () => {
+test.skip('sum', () => {
     //1. Тестовые данные
     const salary: number = 800
     const n: number = 200
@@ -17,23 +17,34 @@ test('sum', () => {
 
 })
 
-test('sub', () => {
+test.skip('sub', () => {
 
     expect(sub(1200, 200)).toBe(1000)
     expect(sub(200, 200)).toBe(0)
 
 })
 
-test('div', () => {
+test.skip('div', () => {
 
     expect(div(1200, 2)).toBe(600)
     expect(div(100, 2)).toBe(50)
 
 })
 
-test('mult', () => {
+test.skip('mult', () => {
 
     expect(mult(1200, 2)).toBe(2400)
     expect(mult(100, 2)).toBe(200)
 })
 
+test.skip('case SUM of salaryReducer', ()=>{
+    const salary: StateType =800
+    const action: ActionType ={
+        type: "SUM",
+        n: 200
+    }
+    const result = salaryReducer(salary, action)
+
+
+    expect(result).toBe(1000)
+})
--- todolist/src/tests/tasks.ts
+++ todolist/src/tests/tasks.ts
@@ -8,3 +8,18 @@ export const div = (salary: number, n: number) => salary / n
 export const mult = (salary: number, n: number) => salary * n
 
 
+export type ActionType = {
+    type: "SUM",
+    n: number
+}
+
+export type StateType = number
+
+export const salaryReducer = (state: StateType, action: ActionType): StateType => {
+    switch (action.type) {
+        case "SUM":
+            return state + action.n
+        default:
+            return state
+    }
+}
